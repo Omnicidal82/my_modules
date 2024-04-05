37c0b61ff802cde6105e456d0dc2f38e27356d5b
Mark C <145363653+Omnicidal82@users.noreply.github.com>
2024-04-05 14:00:34 +0100

    Add files via upload

---
+++ test_save_data.py
@@ -0,0 +1,34 @@
+import pytest
+import json
+from pathlib import Path
+from save_data import save_variable, load_variable, save_variables, load_variables
+
+def test_save_variable():
+    save_variable("test", "var1")
+    path = Path('save_data/var1_data.json')
+    assert path.read_text() == json.dumps("test")
+
+def test_load_variable():
+    save_variable("test", "var1")
+    assert load_variable("var1") == "test"
+
+def test_save_variables():
+    var1 = "test_string"
+    var2 = 42
+    var3 = ["list1", "list2", "list3"]
+    save_variables(var1,var2, var3)
+    path = Path('save_data.json')
+    data_list = json.loads(path.read_text())
+    assert data_list == [
+        {"var1": "test_string"},
+        {"var2": 42},
+        {"var3": ["list1", "list2", "list3"]},
+        ]
+
+def test_load_variables():
+    var1 = "test_string"
+    var2 = 42
+    var3 = ["list1", "list2", "list3"]
+    save_variables(var1,var2, var3)
+    variables = load_variables()
+    assert variables == {'var1': 'test_string', 'var2': 42, 'var3': ['list1', 'list2', 'list3']}
